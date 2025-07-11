66de5aa8394a6bf60fcf5d767c78a8dce7b56191
hadvx <kozakandriy331@gmail.com>
2025-07-11 17:41:28 +0300

    add new sql files in sales, make new structure of repo

---
+++ sales/SQL/Brand-Product Performance(BPP).sql
@@ -0,0 +1,33 @@
+WITH cte AS 
+(
+SELECT 
+	brand
+	, product_class
+	, DATE_TRUNC ('month', transaction_date)::DATE 	AS month
+	, SUM(list_price) 								AS total_sales
+	, SUM(profit) 									AS total_profit
+	, ROUND(AVG(list_price),2) 						AS avg_price
+	, ROUND(AVG(profit),2)							AS avg_profit
+	, SUM(CASE
+			WHEN online_order = 'true'
+			THEN 1
+			ELSE 0 
+		  END) 										AS count_of_online_orders
+FROM sales
+GROUP BY 1,2, DATE_TRUNC ('month', transaction_date)
+) 
+SELECT 
+	c.brand
+	, c.product_class
+	, c.total_sales
+	, c.total_profit
+	, c.avg_price
+	, c.avg_profit
+	, c.count_of_online_orders
+	, c.total_sales - COALESCE(LAG(c.total_sales) OVER(
+		    PARTITION BY c.brand, c.product_class
+   	 		ORDER BY c.month),0)
+											    AS monthly_sales_diff
+FROM cte c
+ORDER BY c.brand, c.product_class, c.month;
+
---
+++ sales/SQL/Regional Sales Insights(RSI).sql
@@ -0,0 +1,41 @@
+WITH total_info AS
+(
+	SELECT  
+		a.state
+		, SUM(s.list_price) 					AS total_sales
+		, SUM(s.profit) 						AS total_profit
+		, COUNT(DISTINCT customer_id) 			AS unique_customers
+		, ROUND(AVG(list_price),2) 				AS avg_invoice
+	FROM address a
+	JOIN sales s USING(customer_id)
+	GROUP BY 1
+)
+
+, top_brands AS 
+(
+	SELECT 
+		a.state
+		, s.brand 								AS top_brands_in_state
+		, count(*) 								AS count_of_sales_by_brands
+		, ROW_NUMBER() OVER (
+			PARTITION BY state 
+			ORDER BY count(*) DESC
+			) 									AS filter
+	FROM sales s
+	JOIN address a USING(customer_id)
+	GROUP BY 1,2
+)
+
+SELECT 
+	tb.state
+	, tb.top_brands_in_state
+	, ti.total_sales
+	, ti.total_profit
+	, ti.avg_invoice
+	, ti.unique_customers
+FROM total_info ti
+JOIN top_brands tb USING (state)
+WHERE tb.filter = 1
+ORDER BY tb.state;
+
+
