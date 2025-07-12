50d8b34107293584d0d1943d90bc95dbb50c1bde
Andriy <kozakandriy331@gmail.com>
2025-07-12 09:33:19 +0300

    Update Regional_Sales_Insights(RSI).sql
    format

--- sales/SQL/Regional_Sales_Insights(RSI).sql
+++ sales/SQL/Regional_Sales_Insights(RSI).sql
@@ -2,10 +2,10 @@ WITH total_info AS --CTE with sum of list_price, profit, unique customers and AV
 (
 	SELECT  
 		a.state
-		, SUM(s.list_price) 					AS total_sales
-		, SUM(s.profit) 						AS total_profit
-		, COUNT(DISTINCT customer_id) 			AS unique_customers
-		, ROUND(AVG(list_price),2) 				AS avg_invoice
+		, SUM(s.list_price) 			AS total_sales
+		, SUM(s.profit) 			AS total_profit
+		, COUNT(DISTINCT customer_id) 		AS unique_customers
+		, ROUND(AVG(list_price),2) 		AS avg_invoice
 	FROM address a
 	JOIN sales s USING(customer_id)
 	GROUP BY 1
@@ -15,12 +15,12 @@ WITH total_info AS --CTE with sum of list_price, profit, unique customers and AV
 (
 	SELECT 
 		a.state
-		, s.brand 								AS top_brands_in_state
-		, count(*) 								AS count_of_sales_by_brands
+		, s.brand 				AS top_brands_in_state
+		, count(*) 				AS count_of_sales_by_brands
 		, ROW_NUMBER() OVER (
 			PARTITION BY state 
 			ORDER BY count(*) DESC
-			) 									AS filter
+			) 				AS filter
 	FROM sales s
 	JOIN address a USING(customer_id)
 	GROUP BY 1,2
