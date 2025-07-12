4f729de52ae9585127e9e67a347b51bb04c39deb
Andriy <kozakandriy331@gmail.com>
2025-07-12 09:29:02 +0300

    Update Brand-Product_Performance(BPP).sql
    add description

--- sales/SQL/Brand-Product_Performance(BPP).sql
+++ sales/SQL/Brand-Product_Performance(BPP).sql
@@ -1,22 +1,23 @@
-WITH cte AS 
-(
-SELECT 
-	brand
-	, product_class
-	, DATE_TRUNC ('month', transaction_date)::DATE 	AS month
-	, SUM(list_price) 								AS total_sales
-	, SUM(profit) 									AS total_profit
-	, ROUND(AVG(list_price),2) 						AS avg_price
-	, ROUND(AVG(profit),2)							AS avg_profit
-	, SUM(CASE
-			WHEN online_order = 'true'
-			THEN 1
-			ELSE 0 
-		  END) 										AS count_of_online_orders
-FROM sales
-GROUP BY 1,2, DATE_TRUNC ('month', transaction_date)
+WITH cte AS         -- CTE with total sales and total profit,  	  
+( 	   	    -- avg price and avg profit,
+	SELECT      -- count online orders by brands plus product class
+		brand
+		, product_class
+		, DATE_TRUNC ('month', transaction_date)::DATE 			AS month
+		, SUM(list_price) 						AS total_sales
+		, SUM(profit) 							AS total_profit
+		, ROUND(AVG(list_price),2) 					AS avg_price
+		, ROUND(AVG(profit),2)						AS avg_profit
+		, SUM(CASE
+				WHEN online_order = 'true'
+				THEN 1
+				ELSE 0 
+			  END) 							AS count_of_online_orders
+	FROM sales
+	GROUP BY 1,2, DATE_TRUNC ('month', transaction_date)
 ) 
-SELECT 
+
+SELECT 		-- select all from cte, add sales difference by month
 	c.brand
 	, c.product_class
 	, c.total_sales
@@ -27,7 +28,7 @@ SELECT
 	, c.total_sales - COALESCE(LAG(c.total_sales) OVER(
 		    PARTITION BY c.brand, c.product_class
    	 		ORDER BY c.month),0)
-											    AS monthly_sales_diff
+									AS monthly_sales_diff
 FROM cte c
 ORDER BY c.brand, c.product_class, c.month;
 
